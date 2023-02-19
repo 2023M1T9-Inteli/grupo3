f87cc570bdc48e5ce018bf7ae18623b5c56f57a1
paulooctavioINTELI <124174379+paulooctavioINTELI@users.noreply.github.com>
2023-02-19 17:51:23 -0300

    Add Test version

---
+++ src/Testes/Teste visual novel/Dialogo.gd
@@ -0,0 +1,33 @@
+extends Node
+
+var dialogo_1 = [
+	{
+		"Name": "Kubayashi",
+		"Expression": "Happy",
+		"Position": "1",
+		"Choices": ["", ""],
+		"Text": "Aqui está você!",
+	},
+	{
+		"Name": "S/N",
+		"Expression": "Neutral",
+		"Position": "3",
+		"Choices": ["", ""],
+		"Text": "Desculpe o atraso!",
+	},
+	{
+		"Name": "Kubayashi",
+		"Expression": "Happy",
+		"Position": "1",
+		"Choices": ["", ""],
+		"Text": "Sem problemas, pelo menos você está aqui agora!",
+	},
+	{
+		"Name": "S/N",
+		"Expression": "Neutral",
+		"Position": "3",
+		"Choices": ["Eu... Eu sinto algo por você", "Eu não sei se é você ou se é o momento"],
+		"Text": "Então... Sobre o que você queria conversar?",
+	},
+	
+]
