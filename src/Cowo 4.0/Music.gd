77066ba59382c5de5ee4ccdbfa51d16f473e556f
paulooctavioINTELI <124174379+paulooctavioINTELI@users.noreply.github.com>
2023-03-20 14:53:43 -0300

    Cowo 4.0

---
+++ src/Cowo 4.0/Music.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var Volume: float = -4
+
+func _process(delta: float) -> void:
+	$AudioStreamPlayer.volume_db = Volume
+	
+	if Global.Media < 5: 
+		$Tween.interpolate_property(
+			$AudioStreamPlayer, "pitch_scale",
+			$AudioStreamPlayer.pitch_scale, convertSpeed(Global.Media), 7,
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
+		)
+		$Tween.start()
+
+func convertSpeed (x: float):
+	return 0.7 + 0.06 * x
