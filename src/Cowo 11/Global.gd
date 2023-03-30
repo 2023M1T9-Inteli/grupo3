71d41844a8707243c6834059e90f045e944e7e8f
paulooctavioINTELI <124174379+paulooctavioINTELI@users.noreply.github.com>
2023-03-30 14:54:50 -0300

    cowo 11

---
+++ src/Cowo 11/Global.gd
@@ -0,0 +1,17 @@
+extends Node
+
+#variavel global que define o nivel de humor de Leticia
+var LeticiaHumor: float = 10.00
+#variavel global que define o nivel de humor de Victor
+var VictorHumor: float = 10.00
+#variavel global que define o nivel de humor de SrFacundes
+var SrFacundesHumor: float = 10.00
+#variavel global que define o nivel de humor de Simon
+var SimonHumor: float = 10.00
+#variavel global que define o nivel de humor médio do escritório
+var Media: float = 10.00
+
+
+var Passiva: int = 0
+var PassivaAgressiva: int = 0
+var Ativa: int = 0
